6dd1cc983a5826c4e79f31816cbcf7739c2f2b73
jampan02 <kusudatu@yahoo.co.jp>
2021-07-12 17:42:38 +0900

    add test

--- backend/tests/Feature/Database/DatabaseTest.php
+++ backend/tests/Feature/Database/DatabaseTest.php
@@ -15,7 +15,7 @@ class DatabaseTest extends TestCase
     {
         $post = new Post();
         $post->user_id=1;
-        //$post->site_name="test_site";
+        $post->site_name="test_site";
 		$post->title="test_title";
 		$post->image="https://unruffled-spence-0404bb.netlify.app/images/posts/%E3%82%A4%E3%83%B3%E3%83%95%E3%83%A9/Docker/1/ogp.png";
 		$post->url="https://unruffled-spence-0404bb.netlify.app/posts/infrastructure/docker/1";
