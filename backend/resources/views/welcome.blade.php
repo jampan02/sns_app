34951e289bf2c1b9ccc72b88daa6fdb33bab8655
jampan02 <kusudatu@yahoo.co.jp>
2021-07-13 07:31:03 +0900

    https allows

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -7,8 +7,6 @@
 
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Laravel</title>
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link rel="stylesheet" href="/css/app.css">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
