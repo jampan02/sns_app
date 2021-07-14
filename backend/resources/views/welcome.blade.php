1aecec2af349abcce351a12bfbfafcc8e8f69baa
jampan02 <kusudatu@yahoo.co.jp>
2021-07-14 19:16:47 +0900

    add meta tags

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -4,8 +4,16 @@
 <html lang="ja">
     <head>
 
+<meta property="og:url" content="https://yuarueru.com" />
+ 
+<meta property="og:type" content="website" />
+ 
+<meta property="og:title" content="yuarueru.com" />
+<meta property="og:site_name" content="ゆあるえる" />
 
+<meta property="og:image" content="/site_icon.png" />
 		<meta charset="utf-8">
+		<meta name="description" content="面白い、勉強になる等のサイトを共有するサイトです。 SNS でもあるため、自分が興味のある投稿をするユーザーをフォローして個別の投稿を閲覧することも可能です。">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="/css/app.css">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
